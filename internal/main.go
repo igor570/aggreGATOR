557da5d3a1f1adebfc5315b83edde604ccafc8fc
Igor Milosavljevic <migor@live.co.uk>
2025-08-03 15:26:01 +0100

    setup command struct

--- internal/main.go
+++ internal/main.go
@@ -1,9 +1,37 @@
 package main
 
+import "fmt"
+
+type state struct {
+	config *Config
+}
+
+type command struct {
+	name      string
+	arguments []string
+}
+
+type commands struct {
+	commands map[string]func(*state, command) error
+}
+
 func main() {
 	config := NewConfig()
 
-	config.ReadConfig()
-	config.SetUser("igor")
-	config.ReadConfig()
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if cmd.arguments == nil {
+		return fmt.Errorf("Command arguments cannot be empty")
+	}
+
+	err := s.config.SetUser(cmd.name)
+
+	if err != nil {
+		fmt.Errorf("%v", err)
+	}
+
+	fmt.Printf("Successfully set the user %v, to the config", cmd.name)
+
+	return nil
 }
